1c2ef978eba7c8a4b50efc565eef3a3c36ecc586
egilltor17 <egilltor17@ru.is>
2019-03-22 16:18:42 +0000

    fixed parser

--- code/main.cpp
+++ code/main.cpp
@@ -8,28 +8,32 @@ using namespace std;
 
 int main()
 {
-//    int scopeCalls = 0;
+    int index = 0;
     string scope = "";
     string call = "";
     unordered_map< string, vector<string> > scopeMap;
     vector<string> callVector;
-    ifstream myfile ("C:\\Users\\Egill\\Documents\\HR\\Önn 4\\Hugbúnaðarfræði II\\Assingments\\Project\\dump.txt");
-    if (myfile.is_open()) {
-        while(getline(myfile,scope)) {
-//            scopeCalls = atoi(scope.substr(scope.find('=', 31), scope.size() - 1));
-//            scopeCalls = atoi("12");
-            scope = scope.substr(31, scope.find('\'', 31) - 31);
-//            cout << scope << "\n";
 
-            while(getline(myfile,call)) {
+    ifstream myfile("./callGraph.out");
+//    ifstream myfile("C:\\Users\\Egill\\Desktop\\bla\\callGraph.txt");
+
+    if(myfile.is_open()) {
+        getline(myfile,scope);          // Eats the "Root of the callGraph"
+        while(getline(myfile,scope)) {  // Outer loop runs through the scopes
+            index = scope.find('\'') + 1;
+            if(index) {
+                scope = scope.substr(index, scope.find('\'', index) - index);
+            } else {
+                scope = "unknown_name";
+            }
+            while(getline(myfile,call)) {   // Inner loop runs though scope's elements
                 if(call == "") {
                     scopeMap.insert({scope, callVector});
                     callVector.clear();
-//                    cout << '\n';
                     break;
                 } else if(call[call.size() - 1] == '\'') {
-                    callVector.push_back(call.substr(32, call.find('\'', 32) - 32));
-//                    cout << '\t' << call.substr(32, call.find('\'', 32) - 32) << "\n";
+                    index = call.find('\'') + 1;
+                    callVector.push_back(call.substr(index, call.find('\'', index) - index));
                 }
             }
         }
