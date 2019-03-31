c9f72058162f6aa1fa7e1f0cfe17ae9a595e0f22
ballihakkari <ballibronze@gmail.com>
2019-03-31 16:44:28 +0000

    fixed main.c

--- main.cpp
+++ main.cpp
@@ -3,58 +3,79 @@
 #include <string>
 #include <set>
 #include <unordered_map>
+#include<stdlib.h>
+
 using namespace std;
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-    int confidance = 65;
-    int support = 0;
+    cout << argc << endl;
+//    cout << argv[0] << endl;
+//    cout << argv[1] << endl;
+//    cout << argv[2] << endl;
+//    cout << argv[3] << endl;
+
+    string fileDir = (argc > 1) ? (string)argv[1] : "./dump.txt";
+    int confidence = (argc == 4) ? atoi(argv[2]) : 65; // Default confidence is 65     - the range [confidence, 1] determines whether or not the bug get reported.
+    int support =    (argc == 4) ? atoi(argv[3]) : 3;  // Default support         - the minimum occurrence of a function pair to be considered as a "bug".
     int index = 0;
-    float conf = (double)confidance/100;
+    cout << "Confidence: " << confidence << endl;
+    cout << "Support: " << support << endl;
+    cout << "Number of args: " << argc << endl;                            // Index of char in string - used to isolate function names form the callGraph data.
+    ifstream myfile(fileDir);
     string scope = "";
     string call = "";
-    unordered_map< string, set<string> > scopeMap;
-    unordered_map< string, int> funcalls;
-    set<string> callSet;
     set<string> allFuncs;
-    ifstream myfile("./dump.txt");
-//    ifstream myfile("C:\\Users\\Egill\\Desktop\\bla\\bla.txt");
-
+    set<string> calledInScope;
+    unordered_map< string, set<string> > funCalls;
+    set<string> scopeAndCount;
+    set<string>::iterator it;
+    set<string>::iterator it2;
+    set<string>::iterator it3;
+    set<string>::iterator itTmp;
+    cout << fileDir << endl;
     if(myfile.is_open()) {
+
         getline(myfile,scope);          // Eats the "Root of the callGraph"
         while(getline(myfile,scope)) {  // Outer loop runs through the scopes
+
+        //cout << fileDir << endl;
             index = scope.find('\'') + 1;
             if(index) {
                 scope = scope.substr(index, scope.find('\'', index) - index);
             } else {
-                scope = "unknown_name";
+                // scope is null function and we skip it
+                while(getline(myfile,call) && call != "") {}
             }
             while(getline(myfile,call)) {   // Inner loop runs though scope's elements
-                if(call == "") {
-                    set<string>::iterator it;
-                    set<string>::iterator it2;
-                    for(it = callSet.begin(); it != callSet.end(); ++it){
-                        set<string>::iterator itTmp = it;
-                        for(it2 = ++itTmp; it2 != callSet.end(); it2++){
+               // cout << fileDir;
+                if(call == "") {            // call is empty string --> scope has ended
+                    for(it = calledInScope.begin(); it != calledInScope.end(); it++){
+                        itTmp = it;
+                        for(it2 = ++itTmp; it2 != calledInScope.end(); it2++){
                             string fun = *it + *it2;
-                            if(funcalls.count(fun)){
-                                funcalls[fun]++;
+                            if(funCalls.count(fun)){
+                                funCalls[fun].insert(scope);
                             } else {
-                                funcalls.insert({fun,1});
+                                scopeAndCount.clear();
+                                scopeAndCount.insert(scope);
+                                funCalls[fun] = scopeAndCount;
                             }
                         }
                     }
-                    callSet.clear();
+                    calledInScope.clear();
                     break;
                 } else if(call[call.size() - 1] == '\'') {
                     index = call.find('\'') + 1;
                     call = call.substr(index, call.find('\'', index) - index);
-                    if(funcalls.count(call)){
-                        funcalls[call]++;
+                    if(funCalls.count(call)){
+                        funCalls[call].insert(scope);
                     } else {
-                        funcalls.insert({call,1});
+                        scopeAndCount.clear();
+                        scopeAndCount.insert(scope);
+                        funCalls[call] = scopeAndCount;
                     }
-                    callSet.insert(call);
                     allFuncs.insert(call);
+                    calledInScope.insert(call);
                 }
             }
         }
@@ -62,16 +83,29 @@ int main(int argc, char** argv)
     } else {
         cout << "Unable to open file\n";
     }
-    set<string>::iterator it;
-    set<string>::iterator it2;
-    for(it = allFuncs.begin(); it != allFuncs.end(); ++it){
-        set<string>::iterator itTmp = it;
+
+    for(it = allFuncs.begin(); it != allFuncs.end(); it++){
+        itTmp = it;
         for(it2 = ++itTmp; it2 != allFuncs.end(); it2++){
             string combo = *it + *it2;
-            double confit1 = ((double)(int)((10000*((double)funcalls[combo])/funcalls[*it] ) + 0.5) / 100);
-            double confit2 = ((double)(int)((10000*((double)funcalls[combo])/funcalls[*it2]) + 0.5) / 100);
-            if(confit1 > confidance && confit1 < 100){cout << "bug: " << *it  << " in " << "TODO:scope" << ", pair: (" << *it << ", " << *it2 << "), support: " << support << ", confidance: " << confit1 << "%\n";}
-            if(confit2 > confidance && confit2 < 100){cout << "bug: " << *it2 << " in " << "TODO:scope" << ", pair: (" << *it << ", " << *it2 << "), support: " << support << ", confidance: " << confit2 << "%\n";}
+            if(support <= funCalls[combo].size()) {
+                double confit1 = ((double)(int)((10000*((double)funCalls[combo].size())/funCalls[*it ].size()) + 0.5) / 100);
+                double confit2 = ((double)(int)((10000*((double)funCalls[combo].size())/funCalls[*it2].size()) + 0.5) / 100);
+                if(confit1 > confidence && confit1 < 100) {
+                    for(it3 = funCalls[*it ].begin(); it3 != funCalls[*it ].end(); it3++) {
+                        if(!funCalls[combo].count(*it3)) {
+                            cout << "bug: " << *it  << " in " << *it3 << ", pair: (" << *it << ", " << *it2 << "), support: " << funCalls[combo].size() << ", confidence: " << confit1 << "%\n";
+                        }
+                    }
+                }
+                if(confit2 > confidence && confit2 < 100) {
+                    for(it3 = funCalls[*it2].begin(); it3 != funCalls[*it2].end(); it3++) {
+                        if(!funCalls[combo].count(*it3)) {
+                            cout << "bug: " << *it2 << " in " << *it3 << ", pair: (" << *it << ", " << *it2 << "), support: " << funCalls[combo].size() << ", confidence: " << confit2 << "%\n";
+                        }
+                    }
+                }
+            }
         }
     }
     return 0;
