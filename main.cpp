7b7e7e2b4cb6d3dd703cd09c34ad33e1b4c7a358
egilltor17 <egilltor17@ru.is>
2019-03-31 20:46:21 +0000

    seams to be working

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <set>
 #include <unordered_map>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 int main(int argc, char* argv[])
 {
-    cout << argc << endl;
+//    cout << argc << endl;
 //    cout << argv[0] << endl;
 //    cout << argv[1] << endl;
 //    cout << argv[2] << endl;
@@ -18,9 +19,9 @@ int main(int argc, char* argv[])
     int confidence = (argc == 4) ? atoi(argv[3]) : 65; // Default confidence is 65     - the range [confidence, 1] determines whether or not the bug get reported.
     int support =    (argc == 4) ? atoi(argv[2]) : 3;  // Default support         - the minimum occurrence of a function pair to be considered as a "bug".
     int index = 0;
-    cout << "Confidence: " << confidence << endl;
-    cout << "Support: " << support << endl;
-    cout << "Number of args: " << argc << endl;                            // Index of char in string - used to isolate function names form the callGraph data.
+//    cout << "Confidence: " << confidence << endl;
+//    cout << "Support: " << support << endl;
+//    cout << "Number of args: " << argc << endl;                            // Index of char in string - used to isolate function names form the callGraph data.
 
     ifstream myfile(fileDir);
     string scope = "";
@@ -35,7 +36,7 @@ int main(int argc, char* argv[])
     set<string>::iterator itTmp;
     if(myfile.is_open()) {
 
-        getline(myfile,scope);          // Eats the "Root of the callGraph"
+//        getline(myfile,scope);          // Eats the "Root of the callGraph"
         while(getline(myfile,scope)) {  // Outer loop runs through the scopes
 
         //cout << fileDir << endl;
@@ -48,13 +49,13 @@ int main(int argc, char* argv[])
             }
             while(getline(myfile,call)) {   // Inner loop runs though scope's elements
                 if(call == "") {            // call is empty string --> scope has ended
-		    scopeAndCount.clear();
-	            scopeAndCount.insert(scope);
                     for(it = calledInScope.begin(); it != calledInScope.end(); it++){
                         itTmp = it;
                         for(it2 = ++itTmp; it2 != calledInScope.end(); it2++){
                             string fun = *it + *it2;
                             if(funCalls.count(fun)){
+                    		scopeAndCount.clear();
+                    		scopeAndCount.insert(scope);
                                 funCalls[fun].insert(scope);
                             } else {
                                 funCalls[fun] = scopeAndCount;
@@ -88,19 +89,19 @@ int main(int argc, char* argv[])
         for(it2 = ++itTmp; it2 != allFuncs.end(); it2++){
             string combo = *it + *it2;
             if(support <= funCalls[combo].size()) {
-                double confit1 = ((double)(int)((10000*((double)funCalls[combo].size())/funCalls[*it ].size()) + 0.5) / 100);
-                double confit2 = ((double)(int)((10000*((double)funCalls[combo].size())/funCalls[*it2].size()) + 0.5) / 100);
+                double confit1 = ((double)(100*funCalls[combo].size())/funCalls[*it ].size());
+                double confit2 = ((double)(100*funCalls[combo].size())/funCalls[*it2].size());
                 if(confit1 > confidence && confit1 < 100) {
                     for(it3 = funCalls[*it ].begin(); it3 != funCalls[*it ].end(); it3++) {
                         if(!funCalls[combo].count(*it3)) {
-                            cout << "bug: " << *it  << " in " << *it3 << ", pair: (" << *it << ", " << *it2 << "), support: " << funCalls[combo].size() << ", confidence: " << confit1 << "%\n";
+                            cout << "bug: " << *it  << " in " << *it3 << ", pair: (" << *it << ", " << *it2 << "), support: " << funCalls[combo].size() << ", confidence: " << fixed << setprecision(2) << confit1 << "%\n";
                         }
                     }
                 }
                 if(confit2 > confidence && confit2 < 100) {
                     for(it3 = funCalls[*it2].begin(); it3 != funCalls[*it2].end(); it3++) {
                         if(!funCalls[combo].count(*it3)) {
-                            cout << "bug: " << *it2 << " in " << *it3 << ", pair: (" << *it << ", " << *it2 << "), support: " << funCalls[combo].size() << ", confidence: " << confit2 << "%\n";
+                            cout << "bug: " << *it2 << " in " << *it3 << ", pair: (" << *it << ", " << *it2 << "), support: " << funCalls[combo].size() << ", confidence: " << fixed << setprecision(2) << confit2 << "%\n";
                         }
                     }
                 }
